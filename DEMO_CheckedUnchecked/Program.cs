26c1f5642813a5f07e36a47e4afb199dff1ae760
Samuel Legrain <samuel.legrain@bstorm.be>
2020-09-03 13:43:30 +0200

    Ajoutez des fichiers projet.

---
+++ DEMO_CheckedUnchecked/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DEMO_CheckedUnchecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j = default;
+            long l = default;
+            i = int.MaxValue;
+            try
+            {
+                j = unchecked(i + 10);
+            }
+            catch(OverflowException ex)
+            {
+                l = i + 10L;
+            }
+            if (l == default)
+            {
+                Console.WriteLine("Voici j : "+j);
+            }
+            else
+            {
+                Console.WriteLine("Voici l : " + l);
+            }
+        }
+    }
+}
